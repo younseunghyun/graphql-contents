6205e1b69fed806e1fb5baf125ee011228ac329c
Jack Yun <rogve22@gmail.com>
2022-07-31 18:51:03 +0900

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,19 @@
+GET /orders
+
+[
+	{
+	  "orderId" : "1",
+	  "totalAmount" : 100000,
+	  "accountId" : "1"
+	},
+	{
+	  "orderId" : "2",
+	  "totalAmount" : 120000,
+	  "accountId" : "1"
+	},
+	{
+	  "orderId" : "3",
+	  "totalAmount" : 200000,
+	  "accountId" : "2"
+	}
+]
